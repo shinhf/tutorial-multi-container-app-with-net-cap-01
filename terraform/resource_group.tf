e67fcf52e9f636eda39b00fbff532cf2c13eeb7d
Kamil Cisewski <kcisewski@magicleap.com>
2022-09-26 03:47:55 +0200

    add folders

--- terraform/resource_group.tf
+++ terraform/resource_group.tf
@@ -18,3 +18,11 @@ resource "azurerm_resource_group" "main" {
     environment = var.env
   }
 }
+
+resource "azurerm_container_registry" "main" {
+  name                = "acr${var.app_name}${var.env}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = "Standard"
+  admin_enabled       = true
+}
